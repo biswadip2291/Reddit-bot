2e3ae0ecc2b9488576f6e59382ee41970841b4aa
Biswadip <biswadip311000456@gmail.com>
2021-06-15 21:58:17 +0530

    initail comit

---
+++ test.py
@@ -0,0 +1,33 @@
+import praw
+topic_name = input("Enter subreddit you want to search: ")
+a = input("Enter keyword you want to search : ")
+b = input("Enter the string to reply to comments : ")
+
+#The credentials can be get by creating an reddit aplicaton
+client_id = "Enter Client_id:"
+client_secret = "Enter Client_Secret:"
+username = "Enter Username:"
+password = "Enter Password:"
+user_agent = "Enter User_Agent:"
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     username=username,
+                     password=password,
+                     user_agent=user_agent)
+sub = reddit.subreddit(topic_name)
+topic_name = sub.hot(limit=10)
+# sub.subscribe()
+for submission in (topic_name):
+    if not submission.stickied:
+     print("*************")
+     print('Title: {}, ups: {}, downs: {}'.format(
+        submission.title, submission.ups, submission.downs))
+    # sub.upvote()
+    for comment in submission.comments:
+     if hasattr(comment, "body"):
+       comment_lower = comment.body.lower()
+       if a in comment_lower:
+        print("------------")
+        print(comment.body)
+        comment.upvote()
+        comment.reply(b)
